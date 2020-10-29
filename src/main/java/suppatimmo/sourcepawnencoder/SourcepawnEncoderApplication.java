d75ff54edef303644fa2d97fbefea64739788fec
suppatimmo <boroszek717@gmail.com>
2020-10-29 15:27:11 +0100

    Initial commit

---
+++ src/main/java/suppatimmo/sourcepawnencoder/SourcepawnEncoderApplication.java
@@ -0,0 +1,12 @@
+package suppatimmo.sourcepawnencoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourcepawnEncoderApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SourcepawnEncoderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/suppatimmo/sourcepawnencoder/SourcepawnEncoderApplicationTests.java
@@ -0,0 +1,13 @@
+package suppatimmo.sourcepawnencoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourcepawnEncoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
